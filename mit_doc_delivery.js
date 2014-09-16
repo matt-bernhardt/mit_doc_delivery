e86414852d7027c6473103f2e526062cef7ec972
Matt Bernhardt <mjbernha@mit.edu>
2014-09-16 10:20:15 -0400

    Adding javascript scan of order page, looking for conditionally-required fields. Necessary because conditional_fields module doesn't actually support required fields - just adds asterisks.
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

---
+++ mit_doc_delivery.js
@@ -0,0 +1,71 @@
+init = function() {
+	jQuery("<div>There are blank required fields dealing with MIT Affiliation and payment methods. Please fill out these fields in order to submit your request.</div>").attr("id","required-message").insertAfter("#edit-buttons");
+	checkFields();
+}
+
+findFields = function(el) {
+	// Finds non-hidden input and textarea fields inside a provided element
+	temp = jQuery(el);
+	collection = [];
+	for(i=0;i<temp.length;i++) {
+		if(isHidden(temp[i])) {
+			collection.push(temp[i]);
+		}
+	}
+	return collection;
+}
+
+checkFields = function() {
+	// First, we need to figure out what's visible
+	col = findFields("#edit-customer-profile-billing-field-mit-status select, #edit-customer-profile-billing-field-mit-department select, #edit-customer-profile-billing-field-mit-degree input, #edit-customer-profile-billing-field-year-granted input, #payment-details input, #payment-details textarea");
+	// Assume valid, and nullify if we find a blank
+	var valid = true;
+	// Loop over found fields, checking for any value
+	for(i=0;i<col.length;i++) {
+		if(!col[i].value || col[i].value === '_none') {
+			// col[i].attr("style:border-color:red;");
+			valid = false;
+		} else {
+			// col[i].attr("style:border-color:auto;");
+		}
+	}
+	// Show/hide submission buttons based on form state
+	if(valid) {
+		jQuery("#required-message").addClass("suppressed");
+		jQuery("#commerce-checkout-form-checkout #edit-buttons").removeClass("suppressed");
+	} else {
+		jQuery("#required-message").removeClass("suppressed");
+		jQuery("#commerce-checkout-form-checkout #edit-buttons").addClass("suppressed");
+	}
+}
+
+// from http://stackoverflow.com/questions/19669786/check-if-element-is-visible-in-dom
+isHidden = function(el) {
+	return !(el.offsetParent === null)
+}
+
+jQuery(document).ready(function() {
+	var foo = 0;
+
+	init();
+
+	jQuery(document).ajaxComplete(function() {
+		// AJAX change (to payment method) - trigger validity check
+		checkFields();
+	});
+
+	// Because #payment-details itself is built and replaced, we have to listen for changes on a parent...
+	jQuery("form#commerce-checkout-form-checkout").change(function(e) {
+		// ...and then determine whether the received change is from that element
+		var target;
+		if(e.srcElement) {
+			target = e.srcElement;
+		} else {
+			target = e.target;
+		}
+		if(jQuery(target).parents("#payment-details").length || jQuery(target).parents(".group-mit")) {
+			// Change to payment reference field - trigger validity check
+			checkFields();
+		}
+	});
+});
