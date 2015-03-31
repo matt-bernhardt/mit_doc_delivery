83633383dfdf1c05ce0caa24c2ec30e6b4601263
Matt Bernhardt <mjbernha@mit.edu>
2015-03-31 16:55:21 -0400

    Changes form validation to support multi-page forms
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- mit_doc_delivery.js
+++ mit_doc_delivery.js
@@ -23,12 +23,14 @@ findFields = function(el) {
 }
 
 checkFields = function() {
+	console.log("Checking fields...");
 	// First, we need to figure out what's visible
 	col = findFields("#edit-customer-profile-billing-field-mit-status select, #edit-customer-profile-billing-field-mit-department select, #edit-customer-profile-billing-field-mit-degree input, #edit-customer-profile-billing-field-year-granted input, #payment-details input, #payment-details textarea");
 	// Assume valid, and nullify if we find a blank
 	var valid = true;
 	// Loop over found fields, checking for any value
 	for(i=0;i<col.length;i++) {
+		// console.log(col[i]);
 		if(!col[i].value || col[i].value === '_none') {
 			// col[i].attr("style:border-color:red;");
 			valid = false;
@@ -38,10 +40,12 @@ checkFields = function() {
 	}
 	// Show/hide submission buttons based on form state
 	if(valid) {
+		console.log("Fields found valid");
 		jQuery("#required-message").addClass("suppressed");
 		jQuery("#commerce-checkout-form-checkout #edit-buttons").removeClass("suppressed");
 		jQuery("#commerce-checkout-form-shipping #edit-buttons").removeClass("suppressed");
 	} else {
+		console.log("Fields found INVALID");
 		jQuery("#required-message").removeClass("suppressed");
 		jQuery("#commerce-checkout-form-checkout #edit-buttons").addClass("suppressed");
 		jQuery("#commerce-checkout-form-shipping #edit-buttons").addClass("suppressed");
@@ -62,6 +66,21 @@ jQuery(document).ready(function() {
 	});
 
 	// Because #payment-details itself is built and replaced, we have to listen for changes on a parent...
+	jQuery("form#commerce-checkout-form-billing").change(function(e) {
+		// ...and then determine whether the received change is from that element
+		var target;
+		if(e.srcElement) {
+			target = e.srcElement;
+		} else {
+			target = e.target;
+		}
+		if(jQuery(target).parents("#payment-details").length || jQuery(target).parents(".group-mit")) {
+			// Change to payment reference field - trigger validity check
+			checkFields();
+		}
+	});
+
+	// Because #payment-details itself is built and replaced, we have to listen for changes on a parent...
 	jQuery("form#commerce-checkout-form-checkout").change(function(e) {
 		// ...and then determine whether the received change is from that element
 		var target;
@@ -75,4 +94,20 @@ jQuery(document).ready(function() {
 			checkFields();
 		}
 	});
+
+	// Because #payment-details itself is built and replaced, we have to listen for changes on a parent...
+	jQuery("form#commerce-checkout-form-shipping").change(function(e) {
+		// ...and then determine whether the received change is from that element
+		var target;
+		if(e.srcElement) {
+			target = e.srcElement;
+		} else {
+			target = e.target;
+		}
+		if(jQuery(target).parents("#payment-details").length) {
+			// Change to payment reference field - trigger validity check
+			checkFields();
+		}
+	});
+
 });
